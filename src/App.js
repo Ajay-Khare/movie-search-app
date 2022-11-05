3235a32c72ed1ecc3677a662bb1a85970cda3cb8
cmanager124 <cmanager124@gmail.com>
2022-11-05 14:31:11 +0530

    ajay

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import './App.css';
-import Cart from './components/cart';
-import Product from './components/products';
+import {useState} from "react"
 function App() {
+  const [data, setData] = useState([])
+  
+  const options = {
+    method: 'GET'
+  };
+
+  fetch('https://www.omdbapi.com/?i=tt3896198&apikey=4968cf38&&s=harry', options)
+    .then(response => response.json())
+    .then(response => console.log(response))
+    .catch(err => console.error(err));
   return (
     <div className="App">
-      <Cart />
-      <Product/>
+      <h1>hiii</h1>
     </div>
   );
 }
